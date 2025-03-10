e688570e152f8fe0d73409dfed35bb642529e352
Caio Matheus Caetano Silva <2401362@SALAS.AULAS>
2025-03-10 19:40:04 -0300

    Segundo arquivo desse projeto

--- aula2lab.py
+++
@@ -1,7 +0,0 @@
-idade = int(input('Digite sua idade zé ruela!!!:  '))
-
-if idade < 18:
-    print('Você não pode fumar vacilão, vai estudar !!! ')
-else: 
-    print('Entra ai, mas sem fumar, estuda mesmo assim !!!')
-
